c18df7fe2daf820f17d0e110c69c21de30fd6fc8
Lexa Vopper <lehandro711@gmail.com>
2021-07-28 21:50:16 +0300

    separete component

--- src/components/EditBoard/TaskInfo/SideBar/Tags.jsx
+++ src/components/EditBoard/TaskInfo/SideBar/Tags.jsx
@@ -2,76 +2,22 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faPen,
-  faTag,
-  faLongArrowAltLeft,
-  faCheck,
-} from '@fortawesome/free-solid-svg-icons';
-import { useParams } from 'react-router-dom';
+import { faPen, faTag } from '@fortawesome/free-solid-svg-icons';
 import { useDispatch, useSelector } from 'react-redux';
-import { useForm } from 'react-hook-form';
 import cn from 'classnames';
-import { createTags } from '../../../Content/SingleBoard/action';
-import { FirebaseContext } from '../../../FirebaseApi';
 import { toggleModalOpen } from '../../../Modal/openModal';
-
 import Modal from '../../../Modal/Modal';
+import { CreateEditTag } from './CreateEditTag';
 
 export const Tags = () => {
   const dispatch = useDispatch();
-  const { register, handleSubmit } = useForm();
-  const firebase = React.useContext(FirebaseContext);
-
   const [mode, createTag] = useState('main');
   const [chosenTag, takeTag] = useState(null);
-  const [position, setColor] = useState(0);
-  const { id } = useParams();
-  const tags = useSelector((state) => state.getBoard?.page?.tags);
-
-  const colors = [
-    'green',
-    '#adad21',
-    'red',
-    '#44014C',
-    '#0079bf',
-    '#d29034',
-    '#519839',
-    '#b04632',
-    '#4a443c',
-    '#51e898',
-    '#b3bac5',
-  ];
+  const tags = useSelector((state) => state.getBoard.page?.tags);
 
   const EditMode = (a) => {
     takeTag(tags[a]);
     createTag('edit');
-  };
-  const changeColor = (index) => {
-    setColor(index);
-    chosenTag.color = colors[index];
-  };
-  const changeMode = (mode) => {
-    setColor(0);
-    takeTag(null);
-    createTag(mode);
-  };
-  const DeleteTag = (tagId) => {
-    delete tags[tagId];
-    createTag('main');
-    firebase.deleteTag(id, tagId);
-  };
-
-  const onSubmit = (data) => {
-    if (mode === 'edit') {
-      dispatch(createTags(id, chosenTag.color, data.title, chosenTag.id));
-      createTag('main');
-    } else {
-      const tagId = Math.round(Math.random() * 100000);
-
-      dispatch(createTags(id, colors[position], data.title, tagId));
-      createTag('main');
-    }
   };
 
   React.useEffect(() => {
@@ -93,11 +39,7 @@ export const Tags = () => {
         )}
         currentId='tags'
       >
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className='form-taskInfo'
-          autoComplete='off'
-        >
+        <div className='form-taskInfo'>
           {mode === 'main' && (
             <>
               <span className='form-taskInfo__title'>Метки</span>
@@ -135,126 +77,13 @@ export const Tags = () => {
               </button>
             </>
           )}
-          {mode === 'create' && (
-            <>
-              <span className='form-taskInfo__title'>Создание метки</span>
-              <span className='form-taskInfo__title-create'>Название</span>
-              <div
-                className='form-taskInfo__back-icon'
-                onClick={() => createTag('main')}
-              >
-                <FontAwesomeIcon
-                  icon={faLongArrowAltLeft}
-                  className='menu-icon'
-                />
-              </div>
-
-              <input {...register('title')} className='form-taskInfo__search' />
-              <span className='form-taskInfo__title-create'>Цвет</span>
-              <ul className='create__colors-tags'>
-                {colors.map((color, index) => (
-                  <li
-                    className='create__block block-tag'
-                    key={color + index}
-                    style={{ backgroundColor: colors[index] }}
-                  >
-                    <div
-                      className={cn('block__layer', {
-                        active: index === position,
-                      })}
-                      onClick={() => setColor(index)}
-                    >
-                      <span>
-                        <FontAwesomeIcon
-                          icon={faCheck}
-                          className='block__layer--icon'
-                        />
-                      </span>
-                    </div>
-                  </li>
-                ))}
-                <div className='block-tag-nocolor'>
-                  <div className='block-tag__text'> Без цвета</div>
-                  <div className='block-tag__description'>
-                    Не будет отображаться на карточке.
-                  </div>
-                </div>
-              </ul>
-              <input
-                type='submit'
-                value='Создать'
-                className={cn('form-taskInfo__submit button', {
-                  active: mode === 'create',
-                })}
-              />
-            </>
-          )}
-          {mode === 'edit' && (
-            <>
-              <span className='form-taskInfo__title'>Изменение метки</span>
-              <span className='form-taskInfo__title-create'>Название</span>
-              <div
-                className='form-taskInfo__back-icon'
-                onClick={() => changeMode('main')}
-              >
-                <FontAwesomeIcon
-                  icon={faLongArrowAltLeft}
-                  className='menu-icon'
-                />
-              </div>
-
-              <input {...register('title')} className='form-taskInfo__search' />
-              <span className='form-taskInfo__title-create'>Цвет</span>
-              <ul className='create__colors-tags'>
-                {colors.map((color, index) => (
-                  <li
-                    className='create__block block-tag'
-                    key={color + index}
-                    style={{ backgroundColor: colors[index] }}
-                  >
-                    <div
-                      className={cn('block__layer', {
-                        active: colors[index] === chosenTag.color,
-                      })}
-                      onClick={() => changeColor(index)}
-                    >
-                      <span>
-                        <FontAwesomeIcon
-                          icon={faCheck}
-                          className='block__layer--icon'
-                        />
-                      </span>
-                    </div>
-                  </li>
-                ))}
-                <div className='block-tag-nocolor'>
-                  <div className='block-tag__text'> Без цвета</div>
-                  <div className='block-tag__description'>
-                    Не будет отображаться на карточке.
-                  </div>
-                </div>
-              </ul>
-              <div className='block-tag__buttons'>
-                <input
-                  type='submit'
-                  value='Сохранить'
-                  className={cn('form-taskInfo__submit button', {
-                    active: mode === 'edit',
-                  })}
-                />
-
-                <button
-                  type='button'
-                  className='form-taskInfo__delete button'
-                  defaultValue='Удалить'
-                  onClick={() => DeleteTag(chosenTag.id)}
-                >
-                  Удалить
-                </button>
-              </div>
-            </>
-          )}
-        </form>
+          <CreateEditTag
+            chosenTag={chosenTag}
+            mode={mode}
+            createTag={createTag}
+            tags={tags}
+          />
+        </div>
       </Modal>
     </div>
   );
